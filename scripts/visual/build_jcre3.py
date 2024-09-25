0017ea1e65b85982a8a7c860654f4544f52482bd
SInadumi <inazumi19@icloud.com>
2024-09-25 23:35:21 +0900

    update scripts

--- scripts/visual/build_jcre3.py
+++ scripts/visual/build_jcre3.py
@@ -1,12 +1,23 @@
 import json
+import logging
+import math
 import os
 from argparse import ArgumentParser
+from collections import defaultdict
 from pathlib import Path
+from typing import Literal
 
 from rhoknp import Document
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+
 
 class ImageTextAugmenter:
+    SPAN_TYPE = Literal[
+        "past-current", "prev-current", "current", "prev-next", "current-next"
+    ]
+
     def __init__(self, dataset_dir: Path) -> None:
         self.dataset_dir = dataset_dir
         _id2cat = json.load(open("./data/categories.json", "r", encoding="utf-8"))
@@ -86,7 +97,79 @@ class ImageTextAugmenter:
                         relation["classId"] = iid2cid[iid]
                     except Exception as e:
                         relation["classId"] = -1  # HACK: dummy category ID
-                        print(f"{scenario_id}: {e.__class__.__name__}: {e}")
+                        logger.warning(f"{scenario_id}: {e.__class__.__name__}: {e}")
+        return annotation
+
+    def add_bboxes_to_phrase_annotations(
+        self, annotation: dict, image_span: SPAN_TYPE
+    ) -> dict:
+        scenario_id = annotation["scenarioId"]
+        image_id_to_annotation = {
+            image["imageId"]: image for image in annotation["images"]
+        }
+
+        dataset_info: dict = json.load(
+            open(
+                self.dataset_dir / "recording" / scenario_id / "info.json",
+                "r",
+                encoding="utf-8",
+            )
+        )
+        if len(dataset_info["utterances"]) != len(annotation["utterances"]):
+            print(len(dataset_info["utterances"]))
+            print("=============================================")
+            print(len(annotation["utterances"]))
+        assert len(dataset_info["utterances"]) == len(annotation["utterances"])
+        a_utterances = annotation["utterances"]
+        d_utterances = dataset_info["utterances"]
+        all_image_ids = [image["id"] for image in dataset_info["images"]]
+        ignore_cnt = 0
+        for idx, (a_utt, d_utt) in enumerate(zip(a_utterances, d_utterances)):
+            sidx = math.ceil(d_utt["start"] / 1000)
+            eidx = math.ceil(d_utt["end"] / 1000)
+
+            if idx >= 1:
+                prev_utterance = d_utterances[idx - 1]
+                prev_eidx = math.ceil(prev_utterance["end"] / 1000)
+            else:
+                prev_eidx = 0
+
+            if idx + 1 < len(d_utterances):
+                next_utterance = d_utterances[idx + 1]
+                next_sdix = math.ceil(next_utterance["start"] / 1000)
+            else:
+                next_sdix = len(d_utterances)
+
+            if image_span == "past-current":
+                image_ids = all_image_ids[:eidx]
+            elif image_span == "prev-current":
+                image_ids = all_image_ids[prev_eidx:eidx]
+            elif image_span == "current":
+                image_ids = all_image_ids[sidx:eidx]
+            elif image_span == "prev-next":
+                image_ids = all_image_ids[prev_eidx:next_sdix]
+            elif image_span == "current-next":
+                image_ids = all_image_ids[sidx:next_sdix]
+            else:
+                raise ValueError(f"Unknown image span: {image_span}")
+
+            instance_id_to_bboxes = defaultdict(lambda: [])
+            for iid in image_ids:
+                for bbox in image_id_to_annotation[iid]["boundingBoxes"]:
+                    instance_id_to_bboxes[bbox["instanceId"]].append(bbox)
+
+            for phrase in a_utt["phrases"]:
+                if len(phrase["relations"]) == 0:
+                    continue
+                for rel in phrase["relations"]:
+                    if rel["instanceId"] in instance_id_to_bboxes:
+                        rel["boundingBox"] = instance_id_to_bboxes[rel["instanceId"]]
+                    else:
+                        ignore_cnt += 1
+                        logger.info(
+                            f"{rel['instanceId']} (in {scenario_id}:{iid}) is ignored."
+                        )
+        logger.info(f"{scenario_id}:{ignore_cnt} instances is ignored.")
         return annotation
 
 
@@ -95,6 +178,9 @@ def main():
     parser.add_argument("INPUT", type=str, help="path to input visual annotation dir")
     parser.add_argument("OUTPUT", type=str, help="path to output dir")
     parser.add_argument("--id", type=str, help="path to id")
+    parser.add_argument(
+        "--image-span", type=str, default="prev-next", help="phrase-grounding range"
+    )
 
     args = parser.parse_args()
 
@@ -124,6 +210,9 @@ def main():
     for source in visual_paths:
         vis_id = source.stem
         image_text_annotation = json.load(open(source, "r", encoding="utf-8"))
+        image_text_annotation = augmenter.add_bboxes_to_phrase_annotations(
+            image_text_annotation, args.image_span
+        )
         image_text_annotation = augmenter.split_utterances_to_sentences(
             image_text_annotation
         )
