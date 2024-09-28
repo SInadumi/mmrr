60f1b74eb8bf4dd923dee6284e8f387befa0fa1d
SInadumi <inazumi19@icloud.com>
2024-09-28 14:36:03 +0900

    change annotation typing

--- src/utils/annotation.py
+++ src/utils/annotation.py
@@ -5,7 +5,7 @@ from pydantic.dataclasses import (
     dataclass,
 )
 
-from utils.util import CamelCaseDataClassJsonMixin, Rectangle
+from .util import CamelCaseDataClassJsonMixin, Rectangle
 
 
 @dataclass
@@ -38,43 +38,42 @@ class DatasetInfo(CamelCaseDataClassJsonMixin):
     images: list[ImageInfo]
 
 
-@dataclass(frozen=True)
+@dataclass
 class BoundingBox(CamelCaseDataClassJsonMixin):
     imageId: str
     instanceId: str
     rect: Rectangle
     className: str
-    classId: int
+    classId: int = -1
 
 
-@dataclass(frozen=True)
+@dataclass
 class Phrase2ObjectRelation(CamelCaseDataClassJsonMixin):
     type: str  # ガ, ヲ, ニ, ノ, =, etc...
     instanceId: str
-    classId: int
+    classId: int = -1
     boundingBox: Optional[list[BoundingBox]] = None  # 発話区間に含まれる bboxes
 
 
-@dataclass(frozen=True)
+@dataclass
 class PhraseAnnotation(CamelCaseDataClassJsonMixin):
     text: str
     relations: list[Phrase2ObjectRelation]
 
 
-@dataclass(frozen=True)
+@dataclass
 class ImageAnnotation(CamelCaseDataClassJsonMixin):
     imageId: str
     boundingBoxes: list[BoundingBox]
 
 
-@dataclass(frozen=True)
+@dataclass
 class SentenceAnnotation(CamelCaseDataClassJsonMixin):
-    sid: str
     text: str
     phrases: list[PhraseAnnotation]
+    sid: Optional[str] = None
 
-
-@dataclass(frozen=True)
+@dataclass
 class ImageTextAnnotation(CamelCaseDataClassJsonMixin):
     scenarioId: str
     images: list[ImageAnnotation]
