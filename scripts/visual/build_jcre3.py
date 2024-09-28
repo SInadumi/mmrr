dc2448f76cb87afb66f1f2bd04bc63ee7cf7fc1d
SInadumi <inazumi19@icloud.com>
2024-09-28 14:37:02 +0900

    change to use annotation typing

--- scripts/visual/build_jcre3.py
+++ scripts/visual/build_jcre3.py
@@ -1,13 +1,18 @@
 import json
 import logging
 import math
-import os
+import shutil
 from argparse import ArgumentParser
 from collections import defaultdict
 from pathlib import Path
 from typing import Literal
 
 from rhoknp import Document
+from src.utils.annotation import (
+    DatasetInfo,
+    ImageTextAnnotation,
+    SentenceAnnotation,
+)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
@@ -28,34 +33,30 @@ class ImageTextAugmenter:
         # '<did>-1-<idx>' -> idx
         return int(sid.split("-")[-1])
 
-    def split_utterances_to_sentences(self, annotation: dict) -> dict:
+    def split_utterances_to_sentences(
+        self, annotation: ImageTextAnnotation
+    ) -> ImageTextAnnotation:
         """visual_annotation/*.jsonの"utterances"エントリとtextual_annotation/*.knpの文分割を揃える処理"""
-        scenario_id = annotation["scenarioId"]
+        scenario_id = annotation.scenarioId
         document = Document.from_knp(
             (
                 self.dataset_dir / "textual_annotations" / f"{scenario_id}.knp"
             ).read_text()
         )
-        dataset_info: dict = json.load(
-            open(
-                self.dataset_dir / "recording" / scenario_id / "info.json",
-                "r",
-                encoding="utf-8",
-            )
+        dataset_info = DatasetInfo.from_json(
+            (self.dataset_dir / "recording" / scenario_id / "info.json").read_text()
         )
 
         # collect sids corresponding to utterances
-        sid_mapper: list[int] = [
-            u_info["sids"] for u_info in dataset_info["utterances"]
-        ]
-        assert len(sid_mapper) == len(annotation["utterances"])
+        sid_mapper: list[int] = [u_info.sids for u_info in dataset_info.utterances]
+        assert len(sid_mapper) == len(annotation.utterances)
 
         # split utterances field
         sentences = []  # [{"sid": xx, "phrases": xx}, ...]
-        for idx, utterance in enumerate(annotation["utterances"]):
+        for idx, utterance in enumerate(annotation.utterances):
             sids = sid_mapper[idx]
             sentences.extend(
-                [{"sid": sid, "phrases": utterance["phrases"]} for sid in sids]
+                [SentenceAnnotation(text="", phrases=utterance.phrases, sid=sid) for sid in sids]
             )
         assert len(sentences) == len(document.sentences)
 
@@ -63,77 +64,71 @@ class ImageTextAugmenter:
         for sentence in document.sentences:
             s_idx = self.to_idx_from_sid(sentence.sid)  # a index of sid
             _s = sentences[s_idx]
-            _s["text"] = sentence.text
-            if len(sentence.base_phrases) != len(_s["phrases"]):
+            _s.text = sentence.text
+            if len(sentence.base_phrases) != len(_s.phrases):
                 # update visual phrase annotation
                 doc_phrase = [b.text for b in sentence.base_phrases]
-                vis_phrase = [u["text"] for u in _s["phrases"]]
+                vis_phrase = [u.text for u in _s.phrases]
                 st_idx = vis_phrase.index(doc_phrase[0])
                 end_idx = st_idx + len(doc_phrase)
-                _s["phrases"] = _s["phrases"][st_idx:end_idx]
-        annotation["utterances"] = sentences
+                _s.phrases = _s.phrases[st_idx:end_idx]
+        annotation.utterances = sentences
 
         return annotation
 
-    def add_class_id(self, annotation: dict) -> dict:
-        scenario_id = annotation["scenarioId"]
+    def add_class_id(self, annotation: ImageTextAnnotation) -> ImageTextAnnotation:
+        scenario_id = annotation.scenarioId
         iid2cid = {}
 
         # add object class id to bounding box annotations
-        for image in annotation["images"]:
-            for bbox in image["boundingBoxes"]:
-                iid = bbox["instanceId"]
-                cid = self.cat2id[bbox["className"]]
-                bbox["classId"] = cid
+        for image in annotation.images:
+            for bbox in image.boundingBoxes:
+                iid = bbox.instanceId
+                cid = self.cat2id[bbox.className]
+                bbox.classId = cid
                 iid2cid[iid] = cid
 
         # add object class id to phrase to object relation annotations
-        for utterance in annotation["utterances"]:
-            for phrase in utterance["phrases"]:
-                for relation in phrase["relations"]:
-                    iid = relation["instanceId"]
+        for utterance in annotation.utterances:
+            for phrase in utterance.phrases:
+                for relation in phrase.relations:
+                    iid = relation.instanceId
                     #  HACK: visual/*.json の "images"中にclassNameが存在しないinstanceIdを回避する例外処理
                     try:
-                        relation["classId"] = iid2cid[iid]
+                        relation.classId = iid2cid[iid]
                     except Exception as e:
-                        relation["classId"] = -1  # HACK: dummy category ID
+                        relation.classId = -1  # HACK: dummy category ID
                         logger.warning(f"{scenario_id}: {e.__class__.__name__}: {e}")
         return annotation
 
     def add_bboxes_to_phrase_annotations(
         self, annotation: dict, image_span: SPAN_TYPE
     ) -> dict:
-        scenario_id = annotation["scenarioId"]
-        image_id_to_annotation = {
-            image["imageId"]: image for image in annotation["images"]
-        }
+        scenario_id = annotation.scenarioId
+        image_id_to_annotation = {image.imageId: image for image in annotation.images}
 
-        dataset_info: dict = json.load(
-            open(
-                self.dataset_dir / "recording" / scenario_id / "info.json",
-                "r",
-                encoding="utf-8",
-            )
+        dataset_info = DatasetInfo.from_json(
+            (self.dataset_dir / "recording" / scenario_id / "info.json").read_text()
         )
 
-        assert len(dataset_info["utterances"]) == len(annotation["utterances"])
-        a_utterances = annotation["utterances"]
-        d_utterances = dataset_info["utterances"]
-        all_image_ids = [image["id"] for image in dataset_info["images"]]
+        assert len(dataset_info.utterances) == len(annotation.utterances)
+        a_utterances = annotation.utterances
+        d_utterances = dataset_info.utterances
+        all_image_ids = [image.id for image in dataset_info.images]
         ignore_cnt = 0
         for idx, (a_utt, d_utt) in enumerate(zip(a_utterances, d_utterances)):
-            sidx = math.ceil(d_utt["start"] / 1000)
-            eidx = math.ceil(d_utt["end"] / 1000)
+            sidx = math.ceil(d_utt.start / 1000)
+            eidx = math.ceil(d_utt.end / 1000)
 
             if idx >= 1:
                 prev_utterance = d_utterances[idx - 1]
-                prev_eidx = math.ceil(prev_utterance["end"] / 1000)
+                prev_eidx = math.ceil(prev_utterance.end / 1000)
             else:
                 prev_eidx = 0
 
             if idx + 1 < len(d_utterances):
                 next_utterance = d_utterances[idx + 1]
-                next_sdix = math.ceil(next_utterance["start"] / 1000)
+                next_sdix = math.ceil(next_utterance.start / 1000)
             else:
                 next_sdix = len(d_utterances)
 
@@ -152,19 +147,19 @@ class ImageTextAugmenter:
 
             instance_id_to_bboxes = defaultdict(lambda: [])
             for iid in image_ids:
-                for bbox in image_id_to_annotation[iid]["boundingBoxes"]:
-                    instance_id_to_bboxes[bbox["instanceId"]].append(bbox)
+                for bbox in image_id_to_annotation[iid].boundingBoxes:
+                    instance_id_to_bboxes[bbox.instanceId].append(bbox)
 
-            for phrase in a_utt["phrases"]:
-                if len(phrase["relations"]) == 0:
+            for phrase in a_utt.phrases:
+                if len(phrase.relations) == 0:
                     continue
-                for rel in phrase["relations"]:
-                    if rel["instanceId"] in instance_id_to_bboxes:
-                        rel["boundingBox"] = instance_id_to_bboxes[rel["instanceId"]]
+                for rel in phrase.relations:
+                    if rel.instanceId in instance_id_to_bboxes:
+                        rel.boundingBox = instance_id_to_bboxes[rel.instanceId]
                     else:
                         ignore_cnt += 1
                         logger.info(
-                            f"{rel['instanceId']} (in {scenario_id}:{iid}) is ignored."
+                            f"{rel.instanceId} (in {scenario_id}:{iid}) is ignored."
                         )
         logger.info(f"{scenario_id}:{ignore_cnt} instances is ignored.")
         return annotation
@@ -199,14 +194,15 @@ def main():
     for source in object_paths:
         obj_id = source.stem
         target = output_root / vis_id2split[obj_id] / f"{obj_id}.pth"
-        os.system(f"cp {source} {target}")
+        shutil.copy(source, target)
 
     # split visual annotations
     visual_paths = visual_dir.glob("*.json")
     augmenter = ImageTextAugmenter(Path(args.INPUT))
     for source in visual_paths:
         vis_id = source.stem
-        image_text_annotation = json.load(open(source, "r", encoding="utf-8"))
+        image_text_annotation = ImageTextAnnotation.from_json(Path(source).read_text())
+        json.load(open(source, "r", encoding="utf-8"))
         image_text_annotation = augmenter.add_bboxes_to_phrase_annotations(
             image_text_annotation, args.image_span
         )
@@ -215,8 +211,8 @@ def main():
         )
         image_text_annotation = augmenter.add_class_id(image_text_annotation)
         target = output_root / vis_id2split[vis_id] / f"{vis_id}.json"
-        json.dump(
-            image_text_annotation, open(target, "w"), indent=1, ensure_ascii=False
+        target.write_text(
+            image_text_annotation.to_json(ensure_ascii=False, indent=2)
         )
 
 
