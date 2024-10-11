0821b1e3772e7723889f6632568a1a0f1c13b2ce
SInadumi <inazumi19@icloud.com>
2024-10-11 18:42:41 +0900

    tweak

--- scripts/visual/postprocess.sh
+++ scripts/visual/postprocess.sh
@@ -29,14 +29,14 @@ do
     echo "Calculate intersection over union (IoU) in J-CRe3 ${split} split ..."
     poetry run python ./scripts/visual/calc_iou_mapper.py \
         "${ROOT_DIR}/jcre3" --dataset-name jcre3 \
-				--object-file-name CLIP_fast_rcnn_R_50_C4_zsinf_w_GT \
-				--split ${split}
+		--object-file-name CLIP_fast_rcnn_R_50_C4_zsinf_w_GT \
+		--split ${split}
 
-		# echo "Calculate intersection over union (IoU) in Flickr30k-Ent-Ja ${split} split ..."
-    # poetry run python ./scripts/visual/calc_iou_mapper.py \
-    #     "${ROOT_DIR}/f30k_ent_jp" --dataset-name f30k_ent_jp \
-		# 		--object-file-name CLIP_fast_rcnn_R_50_C4_zsinf \
-		# 		--split ${split}
+	echo "Calculate intersection over union (IoU) in Flickr30k-Ent-Ja ${split} split ..."
+    poetry run python ./scripts/visual/calc_iou_mapper.py \
+        "${ROOT_DIR}/f30k_ent_jp" --dataset-name f30k_ent_jp \
+		--object-file-name CLIP_fast_rcnn_R_50_C4_zsinf \
+		--split ${split}
 done
 
 echo "done!"
