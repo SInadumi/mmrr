212064abbf4776372fa17755af90f739a6776daa
SInadumi <inazumi19@icloud.com>
2024-10-11 16:14:07 +0900

    add post-process

---
+++ scripts/visual/calc_iou_mapper.py
@@ -0,0 +1,81 @@
+import logging
+from argparse import ArgumentParser
+from pathlib import Path
+
+import h5py
+import numpy as np
+
+from cl_mmref.utils.annotation import (
+    BoundingBox,
+    ImageTextAnnotation,
+)
+from cl_mmref.utils.util import Rectangle, box_iou
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+
+
+def scenario_id_to_iid(scenario_id: str) -> str:
+    return scenario_id.split("-")[-1]
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "ROOT_DIR",
+        type=str,
+        help="path to input/output annotation dir (current project)",
+    )
+    parser.add_argument("--dataset-name", type=str, choices=["jcre3", "f30k_ent_jp"])
+    parser.add_argument(
+        "--object-file-name", type=str, help="name of a hdf5 input file"
+    )
+    parser.add_argument("--split", type=str, choices=["train", "valid", "test"])
+    args = parser.parse_args()
+
+    dataset_dir = Path(args.ROOT_DIR) / args.split
+    object_fp = h5py.File(Path(args.ROOT_DIR) / f"{args.object_file_name}.h5", mode="r")
+    output_fp = h5py.File(
+        Path(args.ROOT_DIR) / f"{args.object_file_name}_iou_mapper.h5", mode="w"
+    )
+
+    visual_paths = dataset_dir.glob("*.json")
+    for source in visual_paths:
+        scenario_id = source.stem
+        if args.dataset_name == "jcre3":
+            image_id = scenario_id.split("-")[-1]
+            orig_scenario_id = "-".join(scenario_id.split("-")[:-1])
+        elif args.dataset_name == "f30k_ent_jp":
+            image_id = scenario_id.split("-")[-1]
+            orig_scenario_id = image_id
+        else:
+            raise ValueError(
+                f"[Scenario id: {scenario_id}] `dataset_name` has an invalid argument {args.dataset_name}"
+            )
+
+        image_text_annotation = ImageTextAnnotation.from_json(Path(source).read_text())
+        assert len(image_text_annotation.images) == 1
+        gold_bboxes: list[BoundingBox] = image_text_annotation.images[0].boundingBoxes
+        predict_bboxes: list[np.ndarray] = list(
+            object_fp[f"{orig_scenario_id}/{image_id}/boxes"]
+        )
+        for gold_bbox in gold_bboxes:
+            for idx, pred_bbox in enumerate(predict_bboxes):
+                _pbb = Rectangle(
+                    x1=pred_bbox[0], y1=pred_bbox[1], x2=pred_bbox[2], y2=pred_bbox[3]
+                )
+                try:
+                    output_fp.create_dataset(
+                        f"{orig_scenario_id}/{image_id}/{gold_bbox.instanceId}/{idx}",
+                        data=box_iou(gold_bbox.rect, _pbb),
+                    )
+                except Exception as e:
+                    logger.warning(
+                        f"{type(e).__name__}: {e}, {orig_scenario_id}/{image_id}/{gold_bbox.instanceId}/{idx}"
+                    )
+    object_fp.close()
+    output_fp.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/visual/postprocess.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -euCo pipefail
+
+readonly ROOT_DIR="${ROOT_DIR:-"./data"}"
+
+usage() {
+    cat << _EOT_
+Usage:
+    ROOT_DIR=data/dataset $0
+
+Options:
+    ROOT_DIR     path to input/output directory
+_EOT_
+    exit 1
+}
+
+if [[ $# -gt 0 ]]; then
+    usage
+fi
+
+if [[ -z "${ROOT_DIR}" ]]; then
+    echo "missing required variable -- ROOT_DIR" >&2
+    usage
+fi
+
+for split in "train" "valid" "test"
+do
+    echo "Calculate intersection over union (IoU) in J-CRe3 ${split} split ..."
+    poetry run python ./scripts/visual/calc_iou_mapper.py \
+        "${ROOT_DIR}/jcre3" --dataset-name jcre3 \
+				--object-file-name CLIP_fast_rcnn_R_50_C4_zsinf_w_GT \
+				--split ${split}
+
+		# echo "Calculate intersection over union (IoU) in Flickr30k-Ent-Ja ${split} split ..."
+    # poetry run python ./scripts/visual/calc_iou_mapper.py \
+    #     "${ROOT_DIR}/f30k_ent_jp" --dataset-name f30k_ent_jp \
+		# 		--object-file-name CLIP_fast_rcnn_R_50_C4_zsinf \
+		# 		--split ${split}
+done
+
+echo "done!"
