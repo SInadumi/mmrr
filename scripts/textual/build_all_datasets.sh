bd8edb1c82f5a45b350021fa861aa3833621de2e
SInadumi <inazumi19@icloud.com>
2024-10-17 19:39:04 +0900

    remove un-used corpus

--- scripts/textual/build_all_datasets.sh
+++ scripts/textual/build_all_datasets.sh
@@ -29,31 +29,26 @@ fi
 
 mkdir -p "${DATASET_ROOT}" "${OUT_DIR}"/{kwdlc,fuman,wac}
 
-# echo "Processing KyotoCorpus ..."
-# poetry run python ./scripts/build_dataset.py "${DATASET_ROOT}"/KyotoCorpus/knp "${OUT_DIR}/kc" \　# cohesion analysisでは未指定
-#   --id "${DATASET_ROOT}/KyotoCorpus/id/full" \
-#   -j "${JOBS}"
+echo "Processing KWDLC ..."
+poetry run python ./scripts/textual/build_dataset.py "${DATASET_ROOT}/KWDLC/knp" "${OUT_DIR}/kwdlc" \
+    --id "${DATASET_ROOT}/KWDLC/id/split_for_pas" \
+    -j "${JOBS}" \
+    --doc-id-format kwdlc
 
-# echo "Processing KWDLC ..."
-# poetry run python ./scripts/textual/build_dataset.py "${DATASET_ROOT}/KWDLC/knp" "${OUT_DIR}/kwdlc" \
-#     --id "${DATASET_ROOT}/KWDLC/id/split_for_pas" \
-#     -j "${JOBS}" \
-#     --doc-id-format kwdlc
+echo "Processing AnnotatedFKCCorpus ..."
+poetry run python ./scripts/textual/build_dataset.py "${DATASET_ROOT}/AnnotatedFKCCorpus/knp" "${OUT_DIR}/fuman" \
+    --id "${DATASET_ROOT}/AnnotatedFKCCorpus/id/split_for_pas" \
+    -j "${JOBS}"
 
-# echo "Processing AnnotatedFKCCorpus ..."
-# poetry run python ./scripts/textual/build_dataset.py "${DATASET_ROOT}/AnnotatedFKCCorpus/knp" "${OUT_DIR}/fuman" \
-#     --id "${DATASET_ROOT}/AnnotatedFKCCorpus/id/split_for_pas" \
-#     -j "${JOBS}"
+echo "Processing WikipediaAnnotatedCorpus ..."
+poetry run python ./scripts/textual/build_dataset.py "${DATASET_ROOT}/WikipediaAnnotatedCorpus/knp" "${OUT_DIR}/wac" \
+    --id "${DATASET_ROOT}/WikipediaAnnotatedCorpus/id" \
+    -j "${JOBS}" --doc-id-format wac
 
-# echo "Processing WikipediaAnnotatedCorpus ..."
-# poetry run python ./scripts/textual/build_dataset.py "${DATASET_ROOT}/WikipediaAnnotatedCorpus/knp" "${OUT_DIR}/wac" \
-#     --id "${DATASET_ROOT}/WikipediaAnnotatedCorpus/id" \
-#     -j "${JOBS}" --doc-id-format wac
-
-# echo "Processing J-CRe3 ..."
-# poetry run python ./scripts/textual/build_dataset.py "${OUT_DIR}/jcre3/knp" "${OUT_DIR}/jcre3" \
-#     --id "${DATASET_ROOT}/J-CRe3/id" \
-#     -j "${JOBS}"
+echo "Processing J-CRe3 ..."
+poetry run python ./scripts/textual/build_dataset.py "${OUT_DIR}/jcre3/knp" "${OUT_DIR}/jcre3" \
+    --id "${DATASET_ROOT}/J-CRe3/id" \
+    -j "${JOBS}"
 
 echo "Processing Flickr30k-Ent-Ja ..."
 poetry run python ./scripts/textual/build_dataset.py "${OUT_DIR}/f30k_ent_jp/knp" "${OUT_DIR}/f30k_ent_jp" \
