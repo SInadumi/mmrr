0c565df5e0039f26d4dcb8c94a04b027cb344ecc
SInadumi <inazumi19@icloud.com>
2024-11-02 10:37:31 +0000

    add mmref writer and prediction script

---
+++ scripts/run_mmref.py
@@ -0,0 +1,123 @@
+import logging
+from pathlib import Path
+from typing import TextIO, Union
+
+import hydra
+import lightning.pytorch as pl
+import torch
+import transformers.utils.logging as hf_logging
+from callbacks import MMRefWriter
+from lightning.pytorch.callbacks import Callback
+from lightning.pytorch.trainer.states import TrainerFn
+from omegaconf import DictConfig, OmegaConf
+from torch.utils.data import DataLoader
+
+from cl_mmref.datamodule.multitask_datamodule import MTDataModule
+from cl_mmref.modules import MMRefModule
+from cl_mmref.utils.util import current_datetime_string
+
+hf_logging.set_verbosity(hf_logging.ERROR)
+logging.getLogger("torch").setLevel(logging.ERROR)
+logging.getLogger("lightning").setLevel(logging.ERROR)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+OmegaConf.register_new_resolver(
+    "now", current_datetime_string, replace=True, use_cache=True
+)
+OmegaConf.register_new_resolver("len", len, replace=True, use_cache=True)
+
+
+class Analyzer:
+    def __init__(self, cfg: DictConfig) -> None:
+        # Load saved model and config
+        self.device = self._prepare_device(device_name="auto")
+        self.model = MMRefModule.load_from_checkpoint(
+            checkpoint_path=hydra.utils.to_absolute_path(cfg.checkpoint),
+            map_location=self.device,
+        )
+        cfg_train: DictConfig = self.model.hparams
+        OmegaConf.set_struct(cfg_train, False)  # enable to add new key-value pairs
+        self.cfg = OmegaConf.merge(cfg_train, cfg)
+        assert isinstance(self.cfg, DictConfig)
+
+        callbacks: list[Callback] = list(
+            map(hydra.utils.instantiate, self.cfg.get("callbacks", {}).values())
+        )
+        self.prediction_writer = MMRefWriter(
+            analysis_target_threshold=self.cfg.analysis_target_threshold,
+        )
+
+        # Instantiate lightning trainer
+        self.trainer: pl.Trainer = hydra.utils.instantiate(
+            self.cfg.trainer,
+            callbacks=[*callbacks, self.prediction_writer],
+            logger=False,
+            devices=self.cfg.devices,
+        )
+
+    @staticmethod
+    def _prepare_device(device_name: str) -> torch.device:
+        n_gpu = torch.cuda.device_count()
+        if device_name == "auto":
+            if n_gpu > 0:
+                device_name = "gpu"
+            else:
+                device_name = "cpu"
+        if device_name == "gpu" and n_gpu == 0:
+            logger.warning(
+                "There's no GPU available on this machine. Using CPU instead."
+            )
+            return torch.device("cpu")
+        else:
+            return torch.device("cuda:0" if device_name == "gpu" else "cpu")
+
+    def gen_dataloader(self, input_dir: Path, object_file_name: str) -> DataLoader:
+        # Instantiate lightning datamodule
+        datamodule_cfg = self.cfg.datamodule
+        datamodule_cfg.predict.data_path = str(input_dir)
+        datamodule_cfg.predict.object_file_name = object_file_name
+        datamodule_cfg.num_workers = int(self.cfg.num_workers)
+        datamodule_cfg.batch_size = int(self.cfg.max_batches_per_device)
+        datamodule = MTDataModule(cfg=datamodule_cfg)
+        datamodule.setup(stage=TrainerFn.PREDICTING)
+        return datamodule.predict_dataloader()
+
+    def analyze(
+        self,
+        dataloader: DataLoader,
+        prediction_destination: Union[Path, TextIO, None] = None,
+        json_destination: Union[Path, TextIO, None] = None,
+    ) -> None:
+        self.prediction_writer.prediction_destination = prediction_destination
+        self.prediction_writer.json_destination = json_destination
+        self.trainer.predict(model=self.model, dataloaders=dataloader)
+
+
+@hydra.main(config_path="../configs/predict", config_name="mmref", version_base=None)
+def main(cfg: DictConfig):
+    if isinstance(cfg.devices, str):
+        cfg.devices = (
+            list(map(int, cfg.devices.split(",")))
+            if "," in cfg.devices
+            else int(cfg.devices)
+        )
+    if isinstance(cfg.max_batches_per_device, str):
+        cfg.max_batches_per_device = int(cfg.max_batches_per_device)
+    if isinstance(cfg.num_workers, str):
+        cfg.num_workers = int(cfg.num_workers)
+
+    analyzer = Analyzer(cfg)
+
+    assert cfg.input_dir is not None
+    assert cfg.export_dir is not None
+
+    source = Path(cfg.input_dir)
+    destination = Path(cfg.export_dir)
+    dataloader = analyzer.gen_dataloader(source, cfg.object_file_name)
+    analyzer.analyze(dataloader, prediction_destination=destination)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/cl_mmref/callbacks/mmref_writer.py
@@ -0,0 +1,130 @@
+import io
+import logging
+from collections.abc import Sequence
+from pathlib import Path
+from typing import Any, Optional, TextIO, Union
+
+import lightning.pytorch as pl
+import numpy as np
+from lightning.pytorch.callbacks import BasePredictionWriter
+from torch.utils.data import Dataset
+from typing_extensions import override
+
+from cl_mmref.datamodule.example import MMRefExample
+from cl_mmref.datasets import MMRefDataset
+from cl_mmref.utils.annotation import SentenceAnnotation
+from cl_mmref.utils.prediction import MMRefPrediction, SentencePrediction
+from cl_mmref.writer.mmref import ProbabilityJsonWriter
+
+logger = logging.getLogger(__name__)
+
+
+class MMRefWriter(BasePredictionWriter):
+    def __init__(
+        self,
+        prediction_destination: Union[Path, TextIO, None] = None,
+        json_destination: Union[Path, TextIO, None] = None,
+        analysis_target_threshold: float = 0.5,
+    ) -> None:
+        super().__init__(write_interval="epoch")
+
+        self.prediction_destination: Union[Path, TextIO, None] = prediction_destination
+        self.json_destination: Union[Path, TextIO, None] = json_destination
+        for dest in (self.prediction_destination, self.json_destination):
+            if dest is None:
+                continue
+            assert isinstance(
+                dest, (Path, TextIO)
+            ), f"destination must be either Path or TextIO, but got {type(dest)}"
+            if isinstance(dest, Path):
+                dest.mkdir(parents=True, exist_ok=True)
+        self.analysis_target_threshold: float = analysis_target_threshold
+
+    def write_on_batch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        prediction: Any,
+        batch_indices: Optional[Sequence[int]],
+        batch: Any,
+        batch_idx: int,
+        dataloader_idx: int,
+    ) -> None:
+        pass
+
+    @override
+    def write_on_epoch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        predictions: Sequence[Any],
+        batch_indices: Optional[Sequence[Any]] = None,
+    ) -> None:
+        dataset: Dataset = trainer.predict_dataloaders.dataset  # type: ignore
+        assert isinstance(dataset, MMRefDataset)
+        json_writer = ProbabilityJsonWriter(dataset)
+        for pred in predictions:
+            batch_example_ids = pred["example_ids"]  # (b)
+            batch_relation_logits = pred["relation_logits"]  # (b, rel, seq, seq)
+            batch_source_mask_logits = pred["source_mask_logits"]  # (b, task, seq)
+            assert (
+                len(batch_relation_logits)
+                == len(batch_example_ids)
+                == len(batch_source_mask_logits)
+            )
+            for example_id, relation_logits, source_mask_logits in zip(
+                batch_example_ids, batch_relation_logits, batch_source_mask_logits
+            ):
+                example: MMRefExample = dataset.examples[example_id.item()]
+                sentences: list[SentenceAnnotation] = [
+                    dataset.sid2vis_sentence[sid] for sid in example.sentence_indices
+                ]
+                # (phrase, rel, candidate)
+                relation_prediction: np.ndarray = dataset.dump_relation_prediction(
+                    relation_logits.cpu().numpy(), example
+                )
+                # (phrase, rel, candidate)
+                candidate_selection_prediction: np.ndarray = np.argsort(
+                    -relation_prediction, axis=2
+                )  # descending order
+                # (phrase, task)
+                source_mask_prediction: np.ndarray = (
+                    dataset.dump_source_mask_prediction(
+                        source_mask_logits.cpu().numpy(), example
+                    )
+                )
+                # (phrase, task)
+                is_analysis_target: np.ndarray = (
+                    source_mask_prediction >= self.analysis_target_threshold
+                )
+
+                sentence_prediction = json_writer.write_sentence_predictions(
+                    example,
+                    sentences,
+                    candidate_selection_prediction.tolist(),
+                    is_analysis_target.tolist(),
+                )
+
+                self.write_prediction(
+                    example.doc_id, example.image_id, sentence_prediction
+                )
+        # TODO: WIP
+        # _ = json_writer.write(sentence_predictions, destination=self.json_destination)
+
+    def write_prediction(
+        self, doc_id: str, image_id: str, prediction: SentencePrediction
+    ) -> None:
+        mmref_prediction = MMRefPrediction(
+            doc_id=doc_id,
+            image_id=image_id,
+            phrases=[phrase for sp in prediction for phrase in sp.phrases],
+        )
+        if isinstance(self.prediction_destination, Path):
+            self.prediction_destination.mkdir(parents=True, exist_ok=True)
+            self.prediction_destination.joinpath(f"{image_id}.json").write_text(
+                mmref_prediction.to_json(indent=2, ensure_ascii=False)
+            )
+        elif isinstance(self.prediction_destination, io.TextIOBase):
+            self.prediction_destination.write(
+                mmref_prediction.to_json(indent=2, ensure_ascii=False)
+            )
