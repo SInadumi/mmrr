779e2236d2e22de7a47335df44749853f0940dfb
SInadumi <inazumi19@icloud.com>
2024-11-20 10:26:47 +0000

    fix typing

--- src/cl_mmref/utils/prediction.py
+++ src/cl_mmref/utils/prediction.py
@@ -4,16 +4,14 @@ import torch
 
 from cl_mmref.utils.util import IGNORE_ID, CamelCaseDataClassJsonMixin, Rectangle
 
-DEFAULT_VIS_EMB_SIZE = 1024
 
-
-@dataclass(frozen=True)
+@dataclass
 class ObjectFeature:
+    feature: torch.Tensor
     image_id: int = IGNORE_ID
     class_id: int = IGNORE_ID
     confidence: float = 0.0
     rect: Rectangle = Rectangle(x1=0, y1=0, x2=0, y2=0)
-    feature: torch.Tensor = torch.zeros(DEFAULT_VIS_EMB_SIZE)
 
 
 @dataclass(eq=True)
