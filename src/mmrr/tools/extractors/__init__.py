e32b144cc284656b2ccc253a89de786be9c0bfb3
SInadumi <inazumi19@icloud.com>
2024-12-01 11:16:14 +0000

    fix to initialize "BaseExtractor"

--- src/mmrr/tools/extractors/__init__.py
+++ src/mmrr/tools/extractors/__init__.py
@@ -1,11 +1,13 @@
+from .base import BaseExtractor
 from .bridging import BridgingExtractor
 from .coreference import CoreferenceExtractor
 from .mmref import MMRefExtractor
 from .pas import PasExtractor
 
 __all__ = [
+    "BaseExtractor",
     "BridgingExtractor",
     "CoreferenceExtractor",
     "PasExtractor",
-    "MMRefExtractor"
+    "MMRefExtractor",
 ]
