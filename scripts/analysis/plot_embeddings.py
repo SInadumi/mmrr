ed24077770d635db283b9db12bb59497138562a8
SInadumi <inazumi19@icloud.com>
2025-07-23 15:53:29 +0900

    update a script

--- scripts/analysis/plot_embeddings.py
+++ scripts/analysis/plot_embeddings.py
@@ -9,13 +9,11 @@ import numpy as np
 import torch
 from omegaconf import OmegaConf
 from rhoknp import Document, Morpheme
-from sklearn.decomposition import FastICA
 from sklearn.manifold import TSNE
 from transformers import AutoConfig, AutoModel, AutoTokenizer
 from transformers.file_utils import PaddingStrategy
 
 OmegaConf.register_new_resolver("len", len, replace=True, use_cache=True)
-POS_CLASSES = ['名詞', '指示詞', '接続詞', '接尾辞', '形容詞', '感動詞', '助詞', '副詞', '接頭辞', '動詞', '特殊']
 
 def save_encoder(config_name: str, target_checkpoint: str, model_path: Path):
     with hydra.initialize(config_path="../../configs"):
@@ -26,22 +24,7 @@ def save_encoder(config_name: str, target_checkpoint: str, model_path: Path):
     )
     torch.save(target_module.encoder.state_dict(), model_path)
 
-def run_ica(mat_X: np.ndarray) -> np.ndarray:
-    # NOTE: https://github.com/shimo-lab/Universal-Geometry-with-ICA/blob/main/geometry/src/ica.py
-    # bar_mat = np.mean(mat_X)
-    # mat_c = mat_X - bar_mat     # centering
-    # pca = PCA(whiten=False)
-    # mat_pca1 = pca.fit_transform(mat_c)
-    # mat_pca2 = mat_pca1 / np.std(mat_pca1, axis=0)  # whitening
-
-    # # ica = FastICA(n_components=2, random_state=0, whiten=False, max_iter=10000, tol=1e-10)
-    # mat_ica = ica.fit_transform(mat_pca2)
-
-    ica = FastICA(n_components=2, random_state=0)
-    mat_ica = ica.fit_transform(mat_X)
-    return mat_ica
-
-def run_tsne(mat_X: np.ndarray, perplexity: float=15.0) -> np.ndarray:
+def run_tsne(mat_X: np.ndarray, perplexity: float=30.0) -> np.ndarray:
     tsne = TSNE(n_components=2, random_state=0, perplexity=perplexity)
     embeddings = tsne.fit_transform(mat_X)
     return embeddings
@@ -81,24 +64,19 @@ def make_embeddings(sentence_level_morphemes: list[list[Morpheme]], model_card: 
     return sentence_level_embeddings
 
 
-def visualize_points(output_path: Path, embedding: np.ndarray, morphemes: list[Morpheme]) -> None:
+def visualize_points(output_path: Path, embedding: np.ndarray, class_ids: list[str]) -> None:
     import plotly
     fig = plotly.graph_objects.Figure()
     assert output_path.suffix == ".pdf"
 
-    for pos in list(POS_CLASSES):
-        plot_indices = []
-        labels = []
-        for idx, morpheme in enumerate(morphemes):
-            if pos == morpheme.pos:
-                plot_indices.append(idx)
-                labels.append(morpheme.text)
+    class_ids_np = np.asarray(class_ids)
+    for cid in np.unique(class_ids_np):
+        plot_indices = np.where(class_ids_np == cid)[0]
         fig.add_trace(
             plotly.graph_objects.Scatter(
                 x=embedding[plot_indices, 0],
                 y=embedding[plot_indices, 1],
                 mode="markers",
-                name=f"{pos}",
             )
         )
     # NOTE: https://github.com/plotly/plotly.py/issues/3469
@@ -126,7 +104,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("--config-name", type=str, default="cohesion")
     parser.add_argument("--model-card", type=str, default="ku-nlp/deberta-v2-large-japanese")
-    parser.add_argument("--target-checkpoint", type=str, help="Path to trained module checkpoint")
+    parser.add_argument("--target-checkpoint", type=str, default=None, help="Path to trained module checkpoint")
     parser.add_argument("--dataset-dir", type=Path, default=Path("data/jcre3"), help="path to annotation dir")
     parser.add_argument("--id-files", type=Path, nargs="+", default=[Path("data/jcre3/id/test.id")], help="Paths to scenario id file")
     parser.add_argument("--output-path", type=Path, default="./data/analysis/debug.pdf")
@@ -136,11 +114,11 @@ def main():
     sentence_level_morphemes: list[list[Morpheme]] = []
     for scenario_id in scenario_ids:
         document = Document.from_knp((args.dataset_dir/ "knp" / f"{scenario_id}.knp").read_text())
-        for sentence in document.sentences:
-            morphemes = [
-                phrase.morphemes[0] for phrase in sentence.base_phrases
-            ] # NOTE: 基本句の先頭の形態素
-            sentence_level_morphemes.append(morphemes)
+
+        # document level
+        sentence_level_morphemes.append(
+            [phrase.morphemes[0] for phrase in document.base_phrases]
+        )
 
     if args.target_checkpoint is not None:
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -151,11 +129,11 @@ def main():
         sentence_level_embeddings = make_embeddings(sentence_level_morphemes=sentence_level_morphemes, model_card=args.model_card)
     morphemes = list(itertools.chain.from_iterable(sentence_level_morphemes))
     embeddings = run_tsne(np.stack(list(itertools.chain.from_iterable(sentence_level_embeddings))))
+    document_ids = [morpheme.document.did for morpheme in morphemes]
 
     output_dir = args.output_path.parent
     output_dir.mkdir(exist_ok=True)
-    visualize_tokens(args.output_path, embeddings, list(itertools.chain.from_iterable(sentence_level_morphemes)))
-
+    visualize_points(args.output_path, embeddings, document_ids)
 
 if __name__ == "__main__":
     main()
